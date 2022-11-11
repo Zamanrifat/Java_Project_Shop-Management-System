5083b9cca557eef1224a0b2a41eeede96232f5fe
MD. RIFAT UZ ZAMAN <77951629+Zamanrifat@users.noreply.github.com>
2022-11-12 03:41:39 +0600

    Add files via upload

---
+++ Interface/InterfaceCustomer.java
@@ -0,0 +1,11 @@
+package interfacesh;
+import java.lang.*;
+public interface InterfaceCustomer
+{
+    public static void disp()
+    {
+        System.out.println("---Inside Interface---");
+    }
+    public void displayinfo();
+   
+}
---
+++ Interface/InterfaceEmployee.java
@@ -0,0 +1,11 @@
+package interfacesh;
+import java.lang.*;
+public interface InterfaceEmployee
+{
+    public static void disp()
+    {
+        System.out.println("---Inside Employee Interface---");
+    }
+    public void showInfo();
+   
+}
